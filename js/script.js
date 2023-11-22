8c26e95b8d82ef64297a329514f854ea5100f91f
MarcoPicca <il96marco@gmail.com>
2023-11-22 13:06:46 +0100

    creazione bombe prima del chiarimento delle 12:55

---
+++ js/script.js
@@ -0,0 +1,73 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella,
+//  la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione:
+// nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati:
+// - abbiamo calpestato una bomba
+// - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti 
+// (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, 
+// cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
+
+
+let buttonGame = document.getElementById('button');
+let mainContentEl = document.querySelector('main div.main-content');
+
+
+
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
+buttonGame.addEventListener('click', function () {
+
+    mainContentEl.innerHTML = '';
+
+    function getNewSquare(){
+        const newSquareElement = document.createElement('article');
+        newSquareElement.classList.add('item-square');
+        return newSquareElement;
+
+    }
+    
+    
+    
+    
+    
+    
+    for (let i = 1; i <= 100; i++){
+        const currentSquare = getNewSquare();
+
+
+        // Ogni cella ha un numero progressivo, da 1 a 100.
+
+        const squareContent = i
+    
+        currentSquare.innerHTML += `<span> ${squareContent} </span>`;
+    
+        currentSquare.addEventListener('click', function (){
+            currentSquare.classList.toggle('clicked');
+            console.log(squareContent);
+        });
+        mainContentEl.appendChild(currentSquare);
+    }
+
+    let bomb = [];
+
+    for (let i = 1; i <= 16; i++){
+
+            bomb.push(i);
+    }
+    if (bomb.length === 16) {
+        console.log(bomb);
+    }
+
+});
